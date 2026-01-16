ef15354684daab3089a70e0ae3f410e991fb91d5
cambriajm <cambriajmorgan@gmail.com>
2026-01-16 08:54:29 -0700

    sounds

--- Example1/Example1/Program.cs
+++ Example1/Example1/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Example1
-{
+﻿// Example1
+//Cam Morgan
+//RCET2265
+//Spring 2026
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
+        Console.Beep(100,700);
+        Console.Read();
         }
     }
-}
