a24219ba318b471151801fcc3635e0881dced57c
cambriajm <cambriajmorgan@gmail.com>
2026-01-20 08:50:31 -0700

    numbers

--- Operators/Operators/Program.cs
+++ Operators/Operators/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int firstNumber = 5;
+            int secondNumber = 10;
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+            Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
+            //Console.WriteLine(5 - 3);
+            //Console.WriteLine(5 * 5);
+            //Console.WriteLine(13 / 5); //integer
+            //Console.WriteLine(13 % 5); //remainder
+            //Console.WriteLine(13d / 5d); //floating point division
+            //Console.WriteLine(13.0 / 5); //floating point division
+            //Console.WriteLine(13 / 5.0); //floating point division
+            //Console.WriteLine(13.0 / 5.0); //floating point division
+
+            //console.writeline(5 > 2);
+            //console.writeline(5 < 2);
+            //console.writeline(5 == 2); //test if equal
+            //console.writeline(5 != 2); //test if not equal
+            //console.writeline(5 >= 2);
+            //console.writeline(5 <= 2);
+            //console.writeline(); // ctrl kc to comment ku to uncomment
+
+            //// compound operators
+            //int a = 5; //assign 5 to variable a
+            ////a = a += 9;
+            //a += 9; //compound assignment of a + 9
+            //Console.WriteLine(a);
+
+            //Console.WriteLine(++a); //do increment on left
+            //Console.WriteLine(a);
+            //Console.WriteLine(a++); //increment on left
+            //Console.WriteLine(a);
+            //Console.WriteLine(++a); //increments and holds previous
+
+            //pause
+            Console.Read();
         }
     }
 }
