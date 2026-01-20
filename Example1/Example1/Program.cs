b5060daa6959bbd0ee43b1ab6fcd49fdb9eecad9
cambriajm <cambriajmorgan@gmail.com>
2026-01-20 08:06:20 -0700

    init

--- Example1/Example1/Program.cs
+++ Example1/Example1/Program.cs
@@ -7,10 +7,22 @@
     {
         static void Main(string[] args)
         {
+        //frequency note define
+        int C4 = 262;
+        int D4 = 294;
+        int E4 = 330;
+        int F4 = 349;
+        int G4 = 392;
+
+
+        //note duration
+        int quarter = 500;//milliseconds
+    
         Console.WriteLine("Hello, World!");
-        Console.Beep(100,700);
-        Console.Beep(200, 900);
-        Console.Beep(200, 900000);
+        Console.WriteLine("Play song");
+        Console.Beep(293,350);
+        Console.Beep(293,350);
+        Console.Beep(587,350);
         Console.Read();
         }
     }
