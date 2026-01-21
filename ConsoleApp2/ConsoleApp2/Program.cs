69812c186edfa215b60366ec024a4e354d5fe49c
cambriajm <cambriajmorgan@gmail.com>
2026-01-21 08:53:23 -0700

    init

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Hello, World!");
+            //Console.Beep(3000, 200)
+            //Console.Beep(200, 30000);
+
+            //string userInput = "hello";
+
+            //Console.WriteLine("Please Type Some Text");
+            //userInput = Console.ReadLine();
+
+            //Console.WriteLine($"User entered: \"{userInput}\""); //easy way
+            //Console.WriteLine("User entered: " + "\"" + userInput + "\""); // harder way same thing
+            string userInput; 
+            Console.WriteLine("Hello! What is your name?");
+            Console.WriteLine($"Hello {userInput}");
+
+
+            Console.Read();
+        }
+    }
+}
