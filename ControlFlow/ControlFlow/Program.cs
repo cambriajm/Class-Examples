8cbb121eba341a5ee855d0526ba6a9226ed4e86a
cambriajm <cambriajmorgan@gmail.com>
2026-01-22 08:21:52 -0700

    samples

---
+++ ControlFlow/ControlFlow/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ControlFlow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 95;
+            if (score >= 90)
+            { 
+                Console.WriteLine("Excellent! You got an A!");
+            }
+
+
+            //pause
+            Console.ReadLine();
+            
+        }
+    }
+}
