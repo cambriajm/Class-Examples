d87963d902162eee80a81c86c82e19b2a38d1363
cambriajm <cambriajmorgan@gmail.com>
2026-01-23 08:24:30 -0700

    waaaa

--- ControlFlow/ControlFlow/Program.cs
+++ ControlFlow/ControlFlow/Program.cs
@@ -44,35 +44,35 @@
 
 
 
-            Console.WriteLine("Please enter a number for the day of the week, 1-7");
-            userInput = Console.ReadLine();
-            switch (userInput)
-            {
-                case "1":
-                    Console.WriteLine("It's Monday. Time to start the week!");
-                    break;
-                case "2":
-                    Console.WriteLine("It's Tuesday. Keep going!");
-                    break;
-                case "3":
-                    Console.WriteLine("It's Wednesday. Halfway there!");
-                    break;
-                case "4":
-                    Console.WriteLine("It's Thursday. Good luck!");
-                    break;
-                case "5":
-                    Console.WriteLine("It's Friday.Awesome!");
-                    break;
-                case "6":
-                    Console.WriteLine("It's Saturday. Weekend!");
-                    break;
-                case "7":
-                    Console.WriteLine("It's Sunday. Ew next week.");
-                    break;
-                default:
-                    Console.WriteLine("It's another day. Make it count!");
-                    break;
-            }
+            //Console.WriteLine("Please enter a number for the day of the week, 1-7");
+            //userInput = Console.ReadLine();
+            //switch (userInput)
+            //{
+            //    case "1":
+            //        Console.WriteLine("It's Monday. Time to start the week!");
+            //        break;
+            //    case "2":
+            //        Console.WriteLine("It's Tuesday. Keep going!");
+            //        break;
+            //    case "3":
+            //        Console.WriteLine("It's Wednesday. Halfway there!");
+            //        break;
+            //    case "4":
+            //        Console.WriteLine("It's Thursday. Good luck!");
+            //        break;
+            //    case "5":
+            //        Console.WriteLine("It's Friday.Awesome!");
+            //        break;
+            //    case "6":
+            //        Console.WriteLine("It's Saturday. Weekend!");
+            //        break;
+            //    case "7":
+            //        Console.WriteLine("It's Sunday. Ew next week.");
+            //        break;
+            //    default:
+            //        Console.WriteLine("It's another day. Make it count!");
+            ////        break;
+            //}
 
                     //pause
                     Console.ReadLine();
