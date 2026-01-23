83620480a06bf68de84701f9031bcf5fb284c943
cambriajm <cambriajmorgan@gmail.com>
2026-01-23 08:50:22 -0700

    added the time loops yippee

---
+++ AnotherExample/AnotherExample/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace AnotherExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine($"Count: {i}");
+            //}
+
+            //for (int i = 5; i >= -5; i--)
+            //{
+            //    Console.WriteLine($"Count: {i}");
+            //}
+
+
+            int number = 1;
+
+            //only runs if condition is true
+            while (number <= 5)
+            {
+                Console.WriteLine($"In While Loop Number: {number}");
+                number++;
+            }//checks before it runs
+
+
+            //always runs at least one time 
+            do
+            {
+                Console.WriteLine($"In Do Loop Number: {number}");
+                number++;
+            } while (number <= 3);
+            //do loop runs once if true. evaluates once. not true. wont run again 
+
+
+
+
+
+            //pause
+            Console.Read();
+
+        }
+    }
+}
