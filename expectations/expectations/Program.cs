27fa9eca1f58082a5d8d5d881004ea4186d4f308
cambriajm <cambriajmorgan@gmail.com>
2026-01-26 08:54:01 -0700

    needs fixed

--- expectations/expectations/Program.cs
+++ expectations/expectations/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace expectations
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace expectations
 {
     internal class Program
     {
@@ -6,21 +8,48 @@
         {
             string userInput = "";
             int result = 0; ;
+            bool isValid = false;
+            string message = "";
 
-            Console.WriteLine("Please enter a number");
-            userInput = Console.ReadLine();
-            Console.WriteLine($"You entered {userInput}");
-            result = int.Parse(userInput);
-            try
+            do
             {
-                result = int.Parse(userInput);
+                Console.WriteLine("Please enter a number");
+                Console.WriteLine("Enter \"Q\" to quit.");
+                userInput = Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine($"You entered {userInput}");
+
+                try 
+                {
+                    result = int.Parse(userInput);
+                    message = $"Good job {userInput} is a number";
+                    isValid = true;
+                }
+                catch (Exception ex)
+                    
+                {
+                    if (userInput == "Q") || userInput == "Q"
+                        
+                        {
+                        (userInput = "Q")
+                    }
+                    message = $"Have a nice day!";
+                    isValid = true;
+                    else
+                    {
+                    message = $"{userInput} is not a number";
+                    }
+                }
+                {
+                    Console.WriteLine($"{userInput} is not a number " + ex.Message); //or stacktrace
+
+                }
+                
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Something went wrong stupid: " + ex.Message); //or stacktrace
-            }
-            //pause
-            Console.Read();
+            while (isValid = false);
+
+            Console.WriteLine($"Good job, you know your numbers, {result} is a number!");
+
         }
     }
 }
