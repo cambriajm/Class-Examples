5f61f00e3576c5d9a8148ea6926b5dd32cb2e655
cambriajm <cambriajmorgan@gmail.com>
2026-01-27 08:41:00 -0700

    numbers random

---
+++ randomnumbers/randomnumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace randomnumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random(); // constructor
+
+            //test randomness and range with a for loop
+            for (int i = 0; i < 10; i++)
+            {
+            int n = rand.Next(1, 5); //random number between one and six
+            
+            Console.WriteLine(n);
+
+            }
+
+            //pause
+            Console.Read();
+        }
+    }
+}
