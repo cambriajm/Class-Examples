90f17598ceb0421b36a7ab6d66473b7b6bdad712
cambriajm <cambriajmorgan@gmail.com>
2026-01-28 08:23:55 -0700

    one dimensional array

--- ArrayExample/ArrayExample/Program.cs
+++ ArrayExample/ArrayExample/Program.cs
@@ -6,9 +6,19 @@
         {
             int thing = 0;
             string[] fruits = new string[7]; 
+            fruits[0] = "apple";
+            fruits[1] = "banana";
+            fruits[2] = "pear";
+            fruits[3] = "pineapple";
+            fruits[4] = "kiwi";
+            fruits[5] = "orange";
+            fruits[6] = "dragonfruit";
+            //fruits[7] = "strawberry";//this will crash YIPPEE
+
+            Console.WriteLine(fruits[3]);
 
 
-            Console.WriteLine("Hello, World!");
+                
             //pause
             Console.Read();
         }
