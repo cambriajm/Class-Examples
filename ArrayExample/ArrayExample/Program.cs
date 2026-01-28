fbef05dd75c2905eb77f5cc66abe9eaf14514e2b
cambriajm <cambriajmorgan@gmail.com>
2026-01-28 08:15:38 -0700

    init

---
+++ ArrayExample/ArrayExample/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ArrayExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int thing = 0;
+            string[] fruits = new string[7]; 
+
+
+            Console.WriteLine("Hello, World!");
+            //pause
+            Console.Read();
+        }
+    }
+}
