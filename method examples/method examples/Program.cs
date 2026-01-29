d8bd67834f4f6ba4f961d440934c5d8441510558
cambriajm <cambriajmorgan@gmail.com>
2026-01-29 08:39:19 -0700

    counting static

---
+++ method examples/method examples/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace method_examples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 5;
+            SayHello();
+            AddNumbers();
+            AddNumbers();
+            SayHello();
+            SayHello();
+            ShowCount();
+            Console.WriteLine(count);
+            //pause
+            Console.Read();
+        }
+        static void SayHello()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static void AddNumbers()
+        {
+            Console.WriteLine($"{1+1}");
+        }
+
+        static void ShowCount()
+        {
+            int count = 7;
+            Console.WriteLine(count);
+        }
+
+    }
+}
