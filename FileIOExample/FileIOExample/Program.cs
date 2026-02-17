34e74b739219fbc30138b40f93f6feb14e4499cd
cambriajm <cambriajm@CAMDELL>
2026-02-17 08:52:30 -0700

    add write. append. read

---
+++ FileIOExample/FileIOExample/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace FileIOExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            WriteToFile();
+            AppendToFile();
+            ReadFile();
+            
+            //pause
+            //Console.Read();
+        }
+
+        static void WriteToFile()
+        {
+            using (StreamWriter testFile = File.CreateText("..\\..\\..\\TestFileYEEHAW.txt"))
+            {
+                testFile.WriteLine("This is where I watched my parents die, Raphael");
+            }
+        }
+        static void AppendToFile()
+        {
+            using (StreamWriter testFile = File.AppendText("..\\..\\..\\TestFileYEEHAW.txt"))
+            {
+                testFile.WriteLine("Cowabummer dude");
+            
+            }
+
+        }
+        static void ReadFile()
+            {
+                using (StreamReader testFileNO = new StreamReader("..\\..\\..\\TestFileYEEHAW.txt"))
+                {
+               
+                Console.WriteLine(testFileNO.ReadLine());
+                Console.WriteLine(testFileNO.ReadLine());
+                    
+
+                }
+            }
+    }
+}
