dbbc60d3156dc954d754ce2a97fa7ea5ca335e68
cambriajm <cambriajm@CAMDELL>
2026-02-18 08:54:40 -0700

    why is it broken

--- FileIOExample/FileIOExample/Program.cs
+++ FileIOExample/FileIOExample/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace FileIOExample
+﻿using System.Diagnostics.Metrics;
+
+namespace FileIOExample
 {
     internal class Program
     {
@@ -9,17 +11,19 @@
             //WriteToFile();
             //AppendToFile();
             //ReadFile();
-            ReadEntireFile("..\\..\\..\\TestFileYEEHAW.txt");
-            
+            //ReadEntireFile("..\\..\\..\\TestFileYEEHAW.txt");
+            //ReadEntireFile("..\\..\\..\\email");
+            FileToArray("..\\..\\..\\email.txt");
+            CountOfLine("..\\..\\..\\email.txt");
             //pause
             //Console.Read();
         }
 
         static void WriteToFile()
         {
-            using (StreamWriter testFile = File.CreateText("..\\..\\..\\TestFileYEEHAW.txt"))
+            using (StreamWriter testFileNO = File.CreateText("..\\..\\..\\TestFileYEEHAW.txt"))
             {
-                testFile.WriteLine("This is where I watched my parents die, Raphael");
+                testFileNO.WriteLine("This is where I watched my parents die, Raphael");
             }
         }
         static void AppendToFile()
@@ -27,21 +31,21 @@
             using (StreamWriter testFile = File.AppendText("..\\..\\..\\TestFileYEEHAW.txt"))
             {
                 testFile.WriteLine("Cowabummer dude");
-            
+
             }
 
         }
         static void ReadFile()
+        {
+            using (StreamReader testFileNO = new StreamReader("..\\..\\..\\TestFileYEEHAW.txt"))
             {
-                using (StreamReader testFileNO = new StreamReader("..\\..\\..\\TestFileYEEHAW.txt"))
-                {
-               
-                Console.WriteLine(testFileNO.ReadLine());
-                Console.WriteLine(testFileNO.ReadLine());
-                    
 
-                }
+                Console.WriteLine(testFileNO.ReadLine());
+                Console.WriteLine(testFileNO.ReadLine());
+
+
             }
+        }
 
         static void ReadEntireFile(string filePath)
         {
@@ -51,10 +55,61 @@
                 do
                 {
                     Console.WriteLine(testFileNO.ReadLine());
-                } while (! testFileNO.EndOfStream);
+                } while (!testFileNO.EndOfStream);
 
 
             }
+        }
+
+        static int CountOfLine(string filePath)
+        {
+            using (StreamReader testFileNO = new StreamReader(filePath))
+            {
+                int count = 0;
+                testFileNO.ReadToEnd();
+                do
+                {
+                    testFileNO.ReadLine();
+                    count++;
+                } while (!testFileNO.EndOfStream);
+
+                return count;
+            }
+
+        }
+        static string[,] FileToArray(string filePath)
+        {
+            int counter = 0;
+            string[,] customerData = new string[5, CountOfLine];
+            string[] temp;
+
+
+            using (StreamReader testFileNO = new StreamReader(filePath))
+            {
+                //testFileNO.ReadToEnd();
+                do
+                {
+                    temp = testFileNO.ReadLine().Split(",");
+
+                    if (temp.Length == 5)
+                    {
+                        temp[0] = temp[0].Replace("\"$$", "");
+                        temp[3] = temp[3].Replace("\""");
+
+                        customerData[0, counter] = temp[0];
+                        customerData[1, counter] = temp[1];
+                        customerData[2, counter] = temp[2];
+                        customerData[3, counter] = temp[3];
+                        //customerData[4, counter] = temp[4];
+                        //customerData[5, counter] = temp[5];
+                    }
+
+                    Console.WriteLine(temp[2]);
+                    counter++;
+                } while (!testFileNO.EndOfStream);
+            }
+            return customerData;
+
         }
     }
 }
