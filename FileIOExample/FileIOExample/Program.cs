1ff035f064e31582ac1bddd9c6aaf88ad9a63e6f
cambriajm <cambriajm@CAMDELL>
2026-02-19 08:50:27 -0700

    finish

--- FileIOExample/FileIOExample/Program.cs
+++ FileIOExample/FileIOExample/Program.cs
@@ -14,9 +14,10 @@ namespace FileIOExample
             //ReadEntireFile("..\\..\\..\\TestFileYEEHAW.txt");
             //ReadEntireFile("..\\..\\..\\email");
             FileToArray("..\\..\\..\\email.txt");
-            CountOfLine("..\\..\\..\\email.txt");
+            DisplayData();
+            
             //pause
-            //Console.Read();
+            Console.Read();
         }
 
         static void WriteToFile()
@@ -61,26 +62,23 @@ namespace FileIOExample
             }
         }
 
-        static int CountOfLine(string filePath)
+        static int CountOfLinesIn(string filePath)
         {
-            using (StreamReader testFileNO = new StreamReader(filePath))
+            int count = 0;
+            using (StreamReader testFile = new StreamReader(filePath))
             {
-                int count = 0;
-                testFileNO.ReadToEnd();
                 do
                 {
-                    testFileNO.ReadLine();
+                    testFile.ReadLine();
                     count++;
-                } while (!testFileNO.EndOfStream);
-
-                return count;
+                } while (!testFile.EndOfStream);
             }
-
+            return count;
         }
         static string[,] FileToArray(string filePath)
         {
             int counter = 0;
-            string[,] customerData = new string[5, CountOfLine];
+            string[,] customerData = new string[4, CountOfLinesIn(filePath)];
             string[] temp;
 
 
@@ -94,8 +92,7 @@ namespace FileIOExample
                     if (temp.Length == 5)
                     {
                         temp[0] = temp[0].Replace("\"$$", "");
-                        temp[3] = temp[3].Replace("\""");
-
+                        temp[3] = temp[3].Replace("\"", "");
                         customerData[0, counter] = temp[0];
                         customerData[1, counter] = temp[1];
                         customerData[2, counter] = temp[2];
@@ -104,12 +101,33 @@ namespace FileIOExample
                         //customerData[5, counter] = temp[5];
                     }
 
-                    Console.WriteLine(temp[2]);
+                    
                     counter++;
                 } while (!testFileNO.EndOfStream);
             }
             return customerData;
 
+        }
+
+        static void DisplayData(string[,] data)
+        {
+            string formattedRow = "";
+
+            for (int row = 0; row < data.GetLength(1); row++)
+            {
+            for (int column = 0; column < data.GetLength(0); column++)
+            {
+                   if (data[column, row] != null)
+                    {
+                        formattedRow += data[column, row].PadRight(14);
+                    }
+            }
+            if(formattedRow != "")
+                {
+                    Console.WriteLine(formattedRow);
+                }
+
+            }
         }
     }
 }
