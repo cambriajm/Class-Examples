c6a1e8e58b0be2195e8dce5b244b1de8e016a3a6
cambriajm <cambriajm@CAMDELL>
2026-02-23 08:46:13 -0700

    commit to the bit

--- WinFormsAppExample/WinFormsAppExample/WinFormExampleForm.Designer.cs
+++ WinFormsAppExample/WinFormsAppExample/WinFormExampleForm.Designer.cs
@@ -28,19 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
+            ExitButton = new Button();
+            SubmitButton = new Button();
+            NameLabel = new Label();
+            NameTextbox = new TextBox();
             SuspendLayout();
             // 
-            // Form1
+            // ExitButton
+            // 
+            ExitButton.BackColor = Color.Cyan;
+            ExitButton.ImageKey = "(none)";
+            ExitButton.Location = new Point(640, 364);
+            ExitButton.Name = "ExitButton";
+            ExitButton.Size = new Size(148, 74);
+            ExitButton.TabIndex = 0;
+            ExitButton.Text = "&Exit";
+            ExitButton.UseVisualStyleBackColor = false;
+            ExitButton.Click += ExitButton_Click;
+            // 
+            // SubmitButton
+            // 
+            SubmitButton.BackColor = Color.CadetBlue;
+            SubmitButton.Location = new Point(486, 364);
+            SubmitButton.Name = "SubmitButton";
+            SubmitButton.Size = new Size(148, 74);
+            SubmitButton.TabIndex = 1;
+            SubmitButton.Text = "&Submit";
+            SubmitButton.UseVisualStyleBackColor = false;
+            SubmitButton.Click += SubmitButton_Click;
+            // 
+            // NameLabel
+            // 
+            NameLabel.AutoSize = true;
+            NameLabel.Location = new Point(201, 121);
+            NameLabel.Name = "NameLabel";
+            NameLabel.Size = new Size(57, 20);
+            NameLabel.TabIndex = 2;
+            NameLabel.Text = "Label 1";
+            // 
+            // NameTextbox
+            // 
+            NameTextbox.Location = new Point(264, 118);
+            NameTextbox.Name = "NameTextbox";
+            NameTextbox.Size = new Size(175, 27);
+            NameTextbox.TabIndex = 3;
+            NameTextbox.TextChanged += this.NameTextbox_TextChanged;
+            // 
+            // WinFormExampleForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Name = "Form1";
+            Controls.Add(NameTextbox);
+            Controls.Add(NameLabel);
+            Controls.Add(SubmitButton);
+            Controls.Add(ExitButton);
+            ForeColor = SystemColors.Desktop;
+            Name = "WinFormExampleForm";
             Text = "Form1";
-            Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button ExitButton;
+        private Button SubmitButton;
+        private Label NameLabel;
+        private TextBox NameTextbox;
+
+        public EventHandler NameTextbox_TextChanged { get; private set; }
     }
 }
--- WinFormsAppExample/WinFormsAppExample/WinFormExampleForm.cs
+++ WinFormsAppExample/WinFormsAppExample/WinFormExampleForm.cs
@@ -7,9 +7,16 @@ namespace WinFormsAppExample
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void ExitButton_Click(object sender, EventArgs e)
         {
-
+            this.Close();
+        }
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            this.Text = NameTextbox.Text;
         }
     }
 }
+
+
+
