9914cb1a29755803dc12f2d2ef58e9fa69532c29
cambriajm <cambriajm@CAMDELL>
2026-02-23 08:18:42 -0700

    git commit winform name

---
+++ WinFormsAppExample/WinFormsAppExample/WinFormExampleForm.cs
@@ -0,0 +1,15 @@
+namespace WinFormsAppExample
+{
+    public partial class WinFormExampleForm : Form
+    {
+        public WinFormExampleForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
